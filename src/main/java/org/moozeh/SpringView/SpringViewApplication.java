d52f2d54e7da9ccb4f06ae5c6fe81bc4d5ab55a7
Chanwoo Kim <kcw2205@naver.com>
2025-02-04 14:42:19 +0900

    Project settings
    - 기초적인 스프링 프로젝트 설정
    - 개발 환경을 위해 일부러 개발 환경은 공개했습니다 (로컬 환경)

---
+++ src/main/java/org/moozeh/SpringView/SpringViewApplication.java
@@ -0,0 +1,13 @@
+package org.moozeh.SpringView;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringViewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringViewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/moozeh/SpringView/SpringViewApplicationTests.java
@@ -0,0 +1,13 @@
+package org.moozeh.SpringView;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringViewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
